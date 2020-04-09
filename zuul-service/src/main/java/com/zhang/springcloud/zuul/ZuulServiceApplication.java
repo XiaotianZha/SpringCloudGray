30c88f3cda5a8fb5356ddf8f02374df4e78b3e91
zxt <bad58n81so@126.com>
2020-04-09 16:50:09 +0800

    zuul init

---
+++ zuul-service/src/main/java/com/zhang/springcloud/zuul/ZuulServiceApplication.java
@@ -0,0 +1,14 @@
+package com.zhang.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+}
